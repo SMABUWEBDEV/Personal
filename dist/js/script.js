bf0a9804941e6b5a43fb1df89679cf34490089a2
SMABUWEBDEV <ichsanabdulaziz@smabudiutomoperak.sch.id>
2023-04-21 08:16:37 +0700

    stock

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,3 +17,16 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+let clientID = "3G3o5OBjyqa9MSygP_oIFu3q0JxdW7LmZ9yZSbjoIRg";
+let endpoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}`;
+
+let imageElement = document.querySelector("#unsplashImage");
+let imageLink = document.querySelector("#imageLink");
+
+fetch(endpoint) 
+    .then(function (response){
+        return response.json();
+    })
+    .then(function(jsonData){
+        imageElement.src = jsonData.urls.small;
+    })
